810273577188fbebe717c5f1677afb1d7f1bae2b
Alejandro Ortiz <maa.ortiz@gmail.com>
2025-07-30 21:56:20 -0500

    Nuevas informacion

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -12,67 +12,67 @@ export class InformacionComponent {
   residuos = [
     { 
       nombre: 'Computadoras',
-      imagen: 'computadoras.jpg',
+      imagen: 'assets/img/computadoras.jpg',
       caracteristicas: ['Computadoras de escritorio', 'Servidores'],
       descripcion: 'De las computadoras como RAEE se extraen metales preciosos, plásticos, circuitos electrónicos, discos duros y otros componentes valiosos para reciclaje.'
     },
     { 
       nombre: 'Fotocopiadoras',
-      imagen: 'fotocopiadora.jpg',
+      imagen: 'assets/img/fotocopiadora.jpg',
       caracteristicas: ['Fotocopiadoras', 'Escáner', 'Impresoras'],
       descripcion: 'De las fotocopiadoras como RAEE se recuperan metales, plásticos, circuitos electrónicos y tóneres, que requieren un reciclaje especializado.'
     },
     { 
       nombre: 'Celulares',
-      imagen: 'celular.jpg',
+      imagen: 'assets/img/celular.jpg',
       caracteristicas: ['Smartphone', 'Celular con teclado', 'Teléfonos'],
       descripcion: 'De los celulares como RAEE se recuperan metales preciosos, baterías, plásticos y circuitos electrónicos para su reciclaje responsable.'
     },
     { 
       nombre: 'Impresoras',
-      imagen: 'impresora.jpg',
+      imagen: 'assets/img/impresora.jpg',
       caracteristicas: ['Impresoras', 'Escanner', 'Fotocopiadoras', 'Multifuncional'],
       descripcion: 'De las impresoras como RAEE se recuperan plásticos, metales, cartuchos de tinta y componentes electrónicos para su reutilización o reciclaje.'
     },
     { 
       nombre: 'Pantallas',
-      imagen: 'pantallas.jpg',
+      imagen: 'assets/img/pantallas.jpg',
       caracteristicas: ['LCD', 'OLED', 'Plasma', 'CRT'],
       descripcion: 'De las pantallas como RAEE se extraen plásticos, vidrios, metales y componentes electrónicos, incluyendo sustancias como fósforo y mercurio en ciertos modelos.'
     },    
     { 
       nombre: 'Módems',
-      imagen: 'modem.jpg',
+      imagen: 'assets/img/modem.jpg',
       caracteristicas: ['Router wifi', 'Switch'],
       descripcion: 'Los módems como RAEE contienen plásticos, placas de circuitos impresos, metales y componentes electrónicos que pueden ser recuperados para su reciclaje.'
     },
     { 
       nombre: 'Tabletas',
-      imagen: 'tabletas.jpg',
+      imagen: 'assets/img/tabletas.jpg',
       caracteristicas: ['Tableta', 'Kindle', 'iPad'],
       descripcion: 'De las tabletas como RAEE se extraen metales preciosos, plásticos, baterías y circuitos electrónicos para su reciclaje y reutilización.'
     },
     { 
       nombre: 'Cables',
-      imagen: 'cables.jpg',
+      imagen: 'assets/img/cables.jpg',
       caracteristicas: ['USB', 'UTP', 'Ethernet', 'Cables eléctricos'],
       descripcion: 'Los cables como RAEE incluyen materiales como cobre, plásticos y metales, que se recuperan y reciclan para su reutilización.'
     },
     { 
       nombre: 'Portátiles',
-      imagen: 'portatil.jpg',
+      imagen: 'assets/img/portatil.jpg',
       caracteristicas: ['Laptop', 'Reproductores DVD'],
       descripcion: 'Los portátiles como RAEE contienen componentes electrónicos, metales y plásticos que se recuperan y reciclan para reducir el impacto ambiental.'
     },
     { 
       nombre: 'Periféricos',
-      imagen: 'perifericos.jpg',
+      imagen: 'assets/img/perifericos.jpg',
       caracteristicas: ['Mouses', 'Teclados', 'Cámaras', 'Parlantes', 'Auriculares', 'Otros'],
       descripcion: 'Los periféricos como RAEE incluyen componentes electrónicos y plásticos, que se extraen y reciclan para minimizar el impacto ambiental.'
     },
     { 
       nombre: 'Piezas y partes',
-      imagen: 'piezas.jpg',
+      imagen: 'assets/img/piezas.jpg',
       caracteristicas: ['Tarjetas SSD', 'Placas RAM', 'MOTHERBOARD', 'Placas wifi', 'Todo tipo de piezas y partes'],
       descripcion: 'Las piezas y partes de dispositivos electrónicos como RAEE incluyen metales preciosos, plásticos y materiales electrónicos que se extraen y reciclan.'
     },
